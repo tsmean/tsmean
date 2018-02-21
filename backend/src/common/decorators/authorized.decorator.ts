43a44236c241334ef9d6361b10fcf6e7f8c3e78e
Michał Lytek <michal.ly@wp.pl>
2018-02-21 13:01:26 +0100

    Fix Authorized decorator overloads

--- backend/src/common/decorators/authorized.decorator.ts
+++ backend/src/common/decorators/authorized.decorator.ts
@@ -4,7 +4,7 @@ import {UserRole} from '../../user/user.role';
 
 export function Authorized(...roles: UserRole[]);
 export function Authorized(roles: UserRole[]);
-export function Authorized(roleOrRoles: UserRole | UserRole[]) {
-    const roles = Array.isArray(roleOrRoles) ? roleOrRoles : [roleOrRoles];
-    return ReflectMetadata('roles', roles);
+export function Authorized(...roleOrRoles: Array<UserRole | UserRole[]>) {
+  const roles = Array.isArray(roleOrRoles[0]) ? (roleOrRoles[0] as UserRole[]) : (roleOrRoles as UserRole[]);
+  return ReflectMetadata('roles', roles);
 }
