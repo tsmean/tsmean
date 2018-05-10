fe003fb22b025cdfbdc861f5b552e0ac6fd54ab0
bersling <bersling@gmail.com>
2018-05-10 12:12:09 +0200

    trying to deploy

--- docker/build-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash bash
-
-# run this from root of project
-docker build -t tsmean/server -f docker/server.dockerfile .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push tsmean/server
-
