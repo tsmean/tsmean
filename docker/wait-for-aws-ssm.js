73dfecb894a946d627b257522b4457163c6ce395
bersling <bersling@gmail.com>
2018-05-11 20:03:14 +0200

    tryna fix again

--- docker/wait-for-aws-ssm.js
+++ docker/wait-for-aws-ssm.js
@@ -2,30 +2,32 @@ const {execSync} = require('child_process');
 const commandOutput = require('./last-command.json');
 
 const getStatus = (commandId) => {
-    const getDetails  = () => {
-        const detailsBuffer = execSync(`aws ssm list-command-invocations --command-id ${commandId} --details`);
-        const detailsStr = detailsBuffer.toString();
-        // This will have a format like { CommandInvokations: [{Status: 'Failed' | 'Pending' | 'Success', CommandPlugins: [{Output: ...}]},...]}
-        const details = JSON.parse(detailsStr);
-        return details;
-    };
     return new Promise((resolve, reject) => {
-        const details = getDetails();
-        const status = details.CommandInvocations[0].Status;
-        if (status === 'Pending' || status === 'InProgress') {
-            setTimeout(getDetails, 100)
-        } else {
-
-console.log('===========')
-console.log('details')
-console.log(details)
-console.log('=============')
-
-            resolve({
-                status: status,
-                output: details.CommandInvocations[0].CommandPlugins[0].Output
-            });
-        }
+        let iterations = 0;
+        const getDetails  = () => {
+            console.log('iteration:', iterations);
+            const detailsBuffer = execSync(`aws ssm list-command-invocations --command-id ${commandId} --details`);
+            const detailsStr = detailsBuffer.toString();
+            // This will have a format like { CommandInvokations: [{Status: 'Failed' | 'Pending' | 'Success', CommandPlugins: [{Output: ...}]},...]}
+            const details = JSON.parse(detailsStr);
+            const status = details.CommandInvocations[0].Status;
+            if (status === 'Pending' || status === 'InProgress') {
+                setTimeout(getDetails, 100)
+                iterations++;
+            } else {
+    
+    console.log('===========')
+    console.log('details')
+    console.log(details)
+    console.log('=============')
+    
+                resolve({
+                    status: status,
+                    output: details.CommandInvocations[0].CommandPlugins[0].Output
+                });
+            }
+        };
+        getDetails();
     });
 };
 
