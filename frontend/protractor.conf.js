01fd57a89d7e8ea0a7a19d8cac592f4e4b0f1e9c
bersling <bersling@gmail.com>
2018-05-21 12:15:04 +0200

    try to add no sandbox option and run e2e tests on CI

--- frontend/protractor.conf.js
+++ frontend/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     chromeOptions: {
-      args: [ "--headless", "--disable-gpu", "--window-size=1200x800" ]
+      args: [ "--headless", "--disable-gpu", "--window-size=1200x800", "--no-sandbox" ]
     }
   },
   directConnect: true,
