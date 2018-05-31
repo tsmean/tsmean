dfe2b366b66d16145a1360a7088560df613749c0
bersling <bersling@gmail.com>
2018-05-31 09:08:41 +0200

    fix frontend tests

--- frontend/src/app/resource/resource.service.spec.ts
+++ frontend/src/app/resource/resource.service.spec.ts
@@ -4,6 +4,15 @@ import {HttpClientTestingModule, HttpTestingController} from '@angular/common/ht
 
 import {ResourceService} from './resource.service';
 import {ApiUrl} from './api-url-injection-token';
+import {Resource, ResourceWithoutId} from '@tsmean/shared';
+
+interface Hero extends Resource {
+  name: string;
+}
+
+interface HeroWithoutId extends ResourceWithoutId {
+  name: string;
+}
 
 describe('ResourceService', () => {
   const apiUrl = 'http://localhost:1234/api/v1/';
@@ -31,7 +40,7 @@ describe('ResourceService', () => {
       // TODO: figure out correct url match
       // mockBackend.expectOne(resourceService.resourcesUrl('heroes')).flush(mockResponse);
 
-      resourceService.getResources('heroes').subscribe(heroes => {
+      resourceService.getResources('heroes').subscribe((heroes: Hero[]) => {
         expect(heroes.length).toBe(2);
         expect(heroes[0].name).toEqual('Wolverine');
         expect(heroes[1].name).toEqual('Wonder Woman');
@@ -49,7 +58,7 @@ describe('ResourceService', () => {
       // TODO: figure out correct url match
       // mockBackend.expectOne(resourceService.resourcesUrl('heroes')).flush(mockResponse);
 
-      resourceService.getResource(0, 'heroes').subscribe(hero => {
+      resourceService.getResource(0, 'heroes').subscribe((hero: Hero) => {
         expect(hero.id).toBe(0);
         expect(hero.name).toBe('Wolverine');
       });
@@ -68,12 +77,12 @@ describe('ResourceService', () => {
 
       resourceService
         .createResource(
-          {
+          <HeroWithoutId>{
             name: 'Wolverine'
           },
           'heroes'
         )
-        .subscribe(hero => {
+        .subscribe((hero: Hero) => {
           expect(hero.id).toBe(0);
           expect(hero.name).toBe('Wolverine');
         });
@@ -92,13 +101,13 @@ describe('ResourceService', () => {
 
       resourceService
         .updateResource(
-          {
+          <Hero>{
             name: 'Stella',
             id: 0
           },
           'heroes'
         )
-        .subscribe(hero => {
+        .subscribe((hero: Hero) => {
           expect(hero.id).toBe(0);
           expect(hero.name).toBe('Stella');
         });
--- frontend/src/app/resource/resource.store.spec.ts
+++ frontend/src/app/resource/resource.store.spec.ts
@@ -2,6 +2,15 @@ import {inject, TestBed} from '@angular/core/testing';
 import {Resource} from '@tsmean/shared';
 
 import {ResourceStoreService} from './resource.store';
+import {ResourceWithoutId} from '../../../../shared';
+
+interface Hero extends Resource {
+  name: string;
+}
+
+interface HeroWithoutId extends ResourceWithoutId {
+  name: string;
+}
 
 describe('Resource Store', () => {
   beforeEach(() => {
@@ -20,37 +29,37 @@ describe('Resource Store', () => {
   it(
     'should be able to create and read',
     inject([ResourceStoreService], (resourceStoreService: ResourceStoreService) => {
-      resourceStoreService.addOrUpdate('heroes', {
+      resourceStoreService.addOrUpdate('heroes', <Hero>{
         id: 413,
         name: 'Wolverine'
       });
       expect(resourceStoreService.get('heroes', 413).getValue().id).toEqual(413);
-      expect(resourceStoreService.get('heroes', 413).getValue().name).toEqual('Wolverine');
+      expect((<Hero>resourceStoreService.get('heroes', 413).getValue()).name).toEqual('Wolverine');
     })
   );
 
   it(
     'should be able to update',
     inject([ResourceStoreService], (resourceStoreService: ResourceStoreService) => {
-      resourceStoreService.addOrUpdate('heroes', {
+      resourceStoreService.addOrUpdate('heroes', <Hero>{
         id: 413,
         name: 'Wolverine'
       });
       expect(resourceStoreService.get('heroes', 413).getValue().id).toEqual(413);
-      expect(resourceStoreService.get('heroes', 413).getValue().name).toEqual('Wolverine');
-      resourceStoreService.addOrUpdate('heroes', {
+      expect((<Hero>resourceStoreService.get('heroes', 413).getValue()).name).toEqual('Wolverine');
+      resourceStoreService.addOrUpdate('heroes', <Hero>{
         id: 413,
         name: 'Wonder Woman'
       });
       expect(resourceStoreService.get('heroes', 413).getValue().id).toEqual(413);
-      expect(resourceStoreService.get('heroes', 413).getValue().name).toEqual('Wonder Woman');
+      expect((<Hero>resourceStoreService.get('heroes', 413).getValue()).name).toEqual('Wonder Woman');
     })
   );
 
   it(
     'should be able to add many',
     inject([ResourceStoreService], (resourceStoreService: ResourceStoreService) => {
-      const heroes: Resource[] = [
+      const heroes: Hero[] = [
         {
           id: 413,
           name: 'Wolverine'
@@ -63,21 +72,21 @@ describe('Resource Store', () => {
 
       resourceStoreService.addOrUpdateMany('heroes', heroes);
       expect(resourceStoreService.get('heroes', 413).getValue().id).toEqual(413);
-      expect(resourceStoreService.get('heroes', 413).getValue().name).toEqual('Wolverine');
+      expect((<Hero>resourceStoreService.get('heroes', 413).getValue()).name).toEqual('Wolverine');
       expect(resourceStoreService.get('heroes', 414).getValue().id).toEqual(414);
-      expect(resourceStoreService.get('heroes', 414).getValue().name).toEqual('Wonder Woman');
+      expect((<Hero>resourceStoreService.get('heroes', 414).getValue()).name).toEqual('Wonder Woman');
     })
   );
 
   it(
     'should be able to delete',
     inject([ResourceStoreService], (resourceStoreService: ResourceStoreService) => {
-      resourceStoreService.addOrUpdate('heroes', {
+      resourceStoreService.addOrUpdate('heroes', <Hero>{
         id: 413,
         name: 'Wolverine'
       });
       expect(resourceStoreService.get('heroes', 413).getValue().id).toEqual(413);
-      expect(resourceStoreService.get('heroes', 413).getValue().name).toEqual('Wolverine');
+      expect((<Hero>resourceStoreService.get('heroes', 413).getValue()).name).toEqual('Wolverine');
       resourceStoreService.remove('heroes', 413);
       // todo: a check for completedness
     })
