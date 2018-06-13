e708628f795c2f08eb6e5aff676a2de81552cbcd
bersling <bersling@gmail.com>
2018-06-13 22:46:07 +0200

    try to terminate on errors...

--- deployment/deploy.travis.sh
+++ deployment/deploy.travis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "${TRAVIS_BRANCH}" = "fix/faster-build" ]; then
-  npm run deploy:backend -- $TRAVIS_BRANCH
-  ./deployment/deploy-frontend.sh
+  npm run deploy:backend -- $TRAVIS_BRANCH || travis_terminate 1
+  ./deployment/deploy-frontend.sh || travis_terminate 1
 fi
 
 echo "DONE DEPLOYING"
