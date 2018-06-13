140b80e31fdd7336d38936202bc597866c53cbac
bersling <bersling@gmail.com>
2018-06-13 21:31:44 +0200

    fix typo...

--- deployment/deploy.travis.sh
+++ deployment/deploy.travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "${TRAVIS_BRANCH}" = "fix/faster-buid" ]; then
+if [ "${TRAVIS_BRANCH}" = "fix/faster-build" ]; then
   npm run deploy:backend -- 220
   ./deployment/deploy-frontend.sh
 fi
