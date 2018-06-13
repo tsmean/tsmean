83fb9369b2418d3767f9265918f1c5ccf4ed17dd
bersling <bersling@gmail.com>
2018-06-13 20:54:09 +0200

    update...

--- deployment/deploy.travis.sh
+++ deployment/deploy.travis.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_BRANCH}" = "fix/faster-buid" ]; then
   npm run deploy:backend -- 220
   ./deployment/deploy-frontend.sh
 fi
 
+echo "DONE DEPLOYING"
