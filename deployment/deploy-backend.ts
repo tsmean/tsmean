576b5a54d3e939aa68fa0411b7d4ed7b721a73e1
bersling <bersling@gmail.com>
2018-06-13 22:23:16 +0200

    actually wait for command to finish...

--- deployment/deploy-backend.ts
+++ deployment/deploy-backend.ts
@@ -1,4 +1,5 @@
 import * as AWS from 'aws-sdk';
+import {CommandId, CommandStatus} from 'aws-sdk/clients/ssm';
 
 AWS.config.update({
   region: 'eu-central-1'
@@ -17,8 +18,40 @@ console.log('Deployment Started');
 const INSTANCE_ID = "i-0ed7d2d2191c0c2d5";
 const TRAVIS_BRANCH = args[0];
 
+const ssm = new AWS.SSM();
+
+const checkStatus = (commandId: CommandId) => {
+  let iterations = 0;
+  const getDetails  = () => {
+    console.log('iteration:', iterations);
+    ssm.listCommandInvocations({
+      CommandId: commandId,
+      Details: true
+    }).promise().then(resp => {
+      const command = resp.CommandInvocations[0];
+      const status: CommandStatus = command.Status;
+      if (status === 'Pending' || status === 'InProgress') {
+        iterations++;
+        getDetails();
+      } else if (status === "Success") {
+        console.log('COMMAND RESULT:');
+        console.log(command);
+        console.log('Command was successful.')
+      } else {
+        console.error('COMMAND RESULT:');
+        console.error(command);
+        console.error(`Result '${status}' was either unhandled or is an error.`)
+      }
+    }).catch(errorResp => {
+      console.error('An error occurrd with the promise to listCommandInvocations.');
+      console.error(errorResp);
+    });
+  };
+  getDetails();
+};
+
 const deployBackend = () => {
-  const ssm = new AWS.SSM();
+
   ssm.sendCommand({
     InstanceIds: [INSTANCE_ID],
     DocumentName: "AWS-RunShellScript",
@@ -26,7 +59,8 @@ const deployBackend = () => {
       commands: [`export TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER && wget --no-cache -O docker-compose.yml https://raw.githubusercontent.com/tsmean/tsmean/"${TRAVIS_BRANCH}"/docker/docker-compose.yml && docker stack deploy --compose-file docker-compose.yml tsmean && rm docker-compose.yml`]
     }
   }).promise().then(resp => {
-    console.log(resp);
+    const commandId = resp.Command.CommandId;
+    checkStatus(commandId);
     console.log('Successfully Deployed Backend')
   }).catch(errorResp => {
     console.error(errorResp);
