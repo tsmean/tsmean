c48c60999dd95942a1bff2cef1c6cd0751a11152
bersling <bersling@gmail.com>
2018-06-13 20:31:29 +0200

    try again

--- deployment/deploy-frontend.sh
+++ deployment/deploy-frontend.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 aws configure set preview.cloudfront true
-aws s3 cp tmp/dist s3://demo.tsmean.com/ --recursive
+aws s3 cp ../frontend/dist s3://demo.tsmean.com/ --recursive
 aws cloudfront create-invalidation --distribution-id E194UGLQ78RUJV --paths "/*"
