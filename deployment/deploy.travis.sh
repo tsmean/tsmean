eff9d1d6b3d5eeb9497e526a5aeecb41c22b08ce
bersling <bersling@gmail.com>
2018-06-13 20:37:24 +0200

    don't use the darn deploy stage

--- deployment/deploy.travis.sh
+++ deployment/deploy.travis.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-npm run deploy:backend -- 220
-./deployment/deploy-frontend.sh
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  npm run deploy:backend -- 220
+  ./deployment/deploy-frontend.sh
+fi
+
