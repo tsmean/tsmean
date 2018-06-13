61619f619f4e901a8c2526b7d8f084f5215fccba
bersling <bersling@gmail.com>
2018-06-13 08:25:38 +0200

    wip some progress with the deployment config

--- deploy.ts
+++ deploy.ts
@@ -1,5 +1,7 @@
 import * as AWS from 'aws-sdk';
 
+AWS.config.update({region: 'eu-central-1'});
+
 const args = process.argv.slice(2);
 if (args.length < 1) {
   console.error(`
@@ -9,7 +11,6 @@ Please Provide a Build Number
   process.exit(1);
 }
 console.log('Deployment Started');
-process.exit(0);
 
 const INSTANCE_ID = "i-0ed7d2d2191c0c2d5";
 const TRAVIS_BRANCH = args[0];
@@ -22,6 +23,12 @@ const deployBackend = () => {
     Parameters: {
       commands: [`export TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER && wget --no-cache -O docker-compose.yml https://raw.githubusercontent.com/tsmean/tsmean/"${TRAVIS_BRANCH}"/docker/docker-compose.yml && docker stack deploy --compose-file docker-compose.yml tsmean && rm docker-compose.yml`]
     }
+  }).promise().then(resp => {
+    console.log(resp);
+    console.log('Successfully Deployed Backend')
+  }).catch(errorResp => {
+    console.error(errorResp);
+    console.error('Error in deploying backend')
   });
 };
 deployBackend();
